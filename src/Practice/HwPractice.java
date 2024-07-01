7dd81145641d66ca94b4a84318f6e46c8322659d
Divyansh Sankhla <divyanshsankhla440@gmail.com>
2024-07-01 23:33:18 +0530

    xpathanddrivers

---
+++ src/Practice/HwPractice.java
@@ -0,0 +1,36 @@
+package Practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HwPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		// To get teh title
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		
+		// Xpath pracice
+//		driver.findElement(By.xpath("//*[@class='ico-register']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Register')]")).click();
+		driver.navigate().back();
+		driver.findElement(By.xpath("//a[contains(text(),'Books')]")).click();
+		
+		List<WebElement> listof=(List<WebElement>) driver.findElement(By.xpath("//*[@class='top-menu']"));
+		
+		System.out.println(listof);
+//		driver.quit();
+	}
+
+}
